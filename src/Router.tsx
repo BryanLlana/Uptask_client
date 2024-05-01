1d117be06eef8350bb474aa0dde2a0647c00fdb9
Bryan Joseph <bryanllana14@gmail.com>
2024-05-01 01:48:44 -0500

    Redirection create and dashboard

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import AppLayout from '@/layouts/AppLayout'
 import ProjectsView from '@/views/ProjectsView'
+import CreateProjectView from './views/projects/CreateProjectView'
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route element={<AppLayout />}>
           <Route path='/' element={<ProjectsView />} index />
+          <Route path='/projects/create' element={<CreateProjectView />}/>
         </Route>
       </Routes>
     </BrowserRouter>
