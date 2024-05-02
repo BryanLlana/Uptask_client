609f61becc9cb47071b8cb309047d9d33a2d617d
Bryan Joseph <bryanllana14@gmail.com>
2024-05-02 12:56:48 -0500

    Get projects added

--- src/services/projectApi.ts
+++ src/services/projectApi.ts
@@ -1,9 +1,20 @@
 import api from "@/lib/axios"
-import { ProjectFormData } from "../types"
+import { Project, ProjectFormData, ProjectsSchema } from "../types"
 
 export const createProject = async (formData: ProjectFormData) => {
   try {
     const { data } = await api.post('/project', formData)
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const getProjects = async () : Promise<Project[] | undefined> => {
+  try {
+    const { data } = await api.get('/project')
+    const response = ProjectsSchema.safeParse(data)
+    if (response.success) return response.data
     return data
   } catch (error) {
     console.log(error)
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,5 +8,14 @@ export const ProjectSchema = z.object({
   description: z.string()
 })
 
+export const ProjectsSchema = z.array(
+  ProjectSchema.pick({
+    _id: true,
+    name: true,
+    customer: true,
+    description: true
+  })
+)
+
 export type Project = z.infer<typeof ProjectSchema>
 export type ProjectFormData = Pick<Project, 'name' | 'customer' | 'description'>
