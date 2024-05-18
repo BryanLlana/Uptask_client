32da6eb7b60a4073ca30ba69456affda635fa273
Bryan Joseph <bryanllana14@gmail.com>
2024-05-18 00:02:34 -0500

    Modal create task added

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import AppLayout from '@/layouts/AppLayout'
 import ProjectsView from '@/views/ProjectsView'
 import CreateProjectView from './views/projects/CreateProjectView'
 import EditProjectView from './views/projects/EditProjectView'
+import ProjectDetailsView from './views/projects/ProjectDetailsView'
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route element={<AppLayout />}>
           <Route path='/' element={<ProjectsView />} index />
           <Route path='/projects/create' element={<CreateProjectView />}/>
+          <Route path='/projects/:id' element={<ProjectDetailsView />}/>
           <Route path='/projects/edit/:id' element={<EditProjectView />}/>
         </Route>
       </Routes>
---
+++ src/views/projects/ProjectDetailsView.tsx
@@ -0,0 +1,38 @@
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+import { useQuery } from '@tanstack/react-query'
+import { getProject } from "@/services/projectApi"
+import Spinner from "@/components/Spinner"
+import CreateTaskModal from "@/components/tasks/CreateTaskModal"
+
+const ProjectDetailsView = () => {
+  const navigate = useNavigate()
+  const { id } = useParams()
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['editProject', id],
+    queryFn: () => getProject(id!),
+    retry: false
+  })
+  
+  
+  if (isLoading) return <Spinner />
+  if (isError) return <Navigate to='/404' />
+  if (data) return (
+    <>
+      <h1 className="text-5xl font-black">{ data.name }</h1>
+      <p className="text-xl font-light text-gray-500 mt-5">{ data.description }</p>
+
+      <nav className="my-5 flex gap-3">
+        <button
+          type="button"
+          className="bg-purple-400 hover:bg-purple-500 px-10 py-3 text-white text-xl font-bold cursor-pointer transition-colors"
+          onClick={() => navigate('?newTask=true')}
+        >Agregar tarea
+        </button>
+      </nav>
+
+      <CreateTaskModal />
+    </>
+  )
+}
+
+export default ProjectDetailsView
